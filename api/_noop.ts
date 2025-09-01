910f9dd25b84db4c07a4ef54e3ddf718059ae02d
Твоё имя <goniponene92978@gmail.com>
2025-09-01 15:09:06 +0400

    test

--- api/_noop.ts
+++ api/_noop.ts
@@ -1,3 +1,5 @@
-export default function handler() {
-  return new Response('ok');
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).send('ok');
 }
