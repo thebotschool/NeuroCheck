c6bfe2ff006649bd981a26029aa938de0d6cb8c7
Твоё имя <goniponene92978@gmail.com>
2025-09-01 15:25:25 +0400

    noop

--- api/noop.mjs
+++ api/noop.mjs
@@ -1,5 +1,3 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   res.status(200).send('ok');
 }
